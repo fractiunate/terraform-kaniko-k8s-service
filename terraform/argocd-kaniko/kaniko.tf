a7ecea6f10920db66cba0020c7e716b7f751ec3a
Fractiunate <david.rahaeuser@gmail.com>
2022-02-07 17:41:37 +0100

    "Terraform Apply" commit

--- terraform/argocd-kaniko/kaniko.tf
+++ terraform/argocd-kaniko/kaniko.tf
@@ -20,6 +20,14 @@ data "template_file" "docker_auth" {
   vars = {
     BASE64_DOCKER_AUTH       = local.base64_docker_auth
   }
+  
+}
+
+data "template_file" "known_hosts" {
+  template = file("${path.module}/known_hosts")
+  provisioner "local-exec" {
+    command = "/bin/bash ssh-keyscan -t rsa github.com >> ${path.module}/known_hosts"
+  }
 }
 
 # Config map to create the config.json file which tells kaniko which cloud provider we are using
@@ -30,6 +38,27 @@ resource "kubernetes_config_map" "docker-config" {
   }
   data = { 
     "config.json" = data.template_file.docker_auth.rendered
+  }
+}
+
+#  ssh-key-secret --from-file=ssh-privatekey=.ssh/id_rsa
+# --from-file=ssh-publickey=.ssh/id_rsa.pub
+# --from-file=known_hosts=known_hosts.github
+# --from-file=config=~/.ssh/config
+
+resource "kubernetes_secret" "kaniko_git_secret" {
+  depends_on = [
+    kubernetes_namespace.kaniko
+  ]
+  metadata {
+    namespace = "kaniko"
+    name = "kaniko-git-secret"
+  }
+  data = {
+    ssh-privatekey-server = var.REGISTRY_SERVER
+    ssh-publickey = var.REGISTRY_USER
+    known_hosts = var.REGISTRY_PASS
+    config = var.REGISTRY_EMAIL
   }
 }
 
